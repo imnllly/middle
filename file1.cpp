6edf522aa9c7ed55a727dc52bb07dd2561bdb6d9
imnllly <143996748+imnllly@users.noreply.github.com>
2023-09-15 22:02:53 +0300

    file1.cpp

--- file1.cpp
+++ file1.cpp
@@ -22,8 +22,10 @@ int itc_len_num(long long number) {
 }
 int itc_sum_num(long long number) {
 	int sum = 0;
+	if (number < 0) number = -number;
 	while (number > 0) {
 		sum += (number % 10);
-
+		number /= 10;
 	}
+	return sum;
 }
