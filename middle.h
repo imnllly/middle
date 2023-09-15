675efe702fdcb0339f8c7f0c22a265a13b842cbb
imnllly <143996748+imnllly@users.noreply.github.com>
2023-09-15 22:45:58 +0300

    middle.h

--- middle.h
+++ middle.h
@@ -4,5 +4,8 @@
 void itc_num_print(int number);
 int itc_len_num(long long number);
 int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+int itc_min_num(long long number);
 
 #endif
